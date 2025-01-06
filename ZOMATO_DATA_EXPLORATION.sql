f34e8447329d2d469b6c745986fe8dfeb4cff8ee
ghostfacebillaah <93541333+ghostfacebillaah@users.noreply.github.com>
2025-01-06 22:51:51 +0530

    Update ZOMATO_DATA_EXPLORATION.sql

--- ZOMATO_DATA_EXPLORATION.sql
+++ ZOMATO_DATA_EXPLORATION.sql
@@ -1,177 +1,143 @@
+-- Check Datatype of table
+DESCRIBE ZomatoData1;
 
-USE [project];
+-- Check tables in all the databases
+SELECT DISTINCT(TABLE_SCHEMA), TABLE_NAME FROM INFORMATION_SCHEMA.COLUMNS;
 
-SELECT 
-COLUMN_NAME, 
-DATA_TYPE 
-FROM INFORMATION_SCHEMA.COLUMNS
-where TABLE_NAME = 'ZomatoData1'																				-- Check Datatype of table
+-- List all columns in all tables
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS;
 
+-- Select all records from ZomatoData1
+SELECT * FROM ZomatoData1;
 
-SELECT DISTINCT(TABLE_CATALOG),TABLE_NAME FROM INFORMATION_SCHEMA.COLUMNS										-- CHECK TABLES IN ALL THE DATABSE
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+-- Checking for duplicate RestaurantIDs
+SELECT RestaurantID, COUNT(RestaurantID)
+FROM ZomatoData1
+GROUP BY RestaurantID
+ORDER BY 2 DESC;
 
-SELECT * FROM [dbo].[ZomatoData1]
-
-
-
---CHECKING FOR DUPLICATE
-SELECT [RestaurantID],COUNT([RestaurantID]) FROM 
-[dbo].[ZomatoData1]
-GROUP BY [RestaurantID]
-ORDER BY 2 DESC
-
+-- Checking data from ZOMATO_COUNTRY table
 SELECT * FROM ZOMATO_COUNTRY;
 
+-- Removing unwanted rows
+DELETE FROM ZomatoData1
+WHERE CountryCode IN (' Bar', ' Grill', ' Bakers & More', ' Chowringhee Lane', ' Grill & Bar', ' Chinese');
 
+DELETE FROM ZomatoData1
+WHERE RestaurantID = '18306543';
 
---REMOVING UNWANTED ROWS
-DELETE FROM [dbo].[ZomatoData1] 
-WHERE [CountryCode] IN (' Bar',' Grill',' Bakers & More"',' Chowringhee Lane"',' Grill & Bar"',' Chinese')
+-- Country Code column
+SELECT A.CountryCode, B.COUNTRY
+FROM ZomatoData1 A 
+JOIN ZOMATO_COUNTRY B ON A.CountryCode = B.COUNTRYCODE;
 
-DELETE FROM [dbo].[ZomatoData1] 
-WHERE [RestaurantID] = '18306543'
+-- Add Country Name column
+ALTER TABLE ZomatoData1 ADD COLUMN COUNTRY_NAME VARCHAR(50);
 
-SELECT * FROM [dbo].[ZomatoData1]
+-- Update Country Name column
+UPDATE ZomatoData1 A
+JOIN ZOMATO_COUNTRY B ON A.CountryCode = B.COUNTRYCODE
+SET A.COUNTRY_NAME = B.COUNTRY;
 
+-- City column
+SELECT DISTINCT City 
+FROM ZomatoData1
+WHERE City LIKE '%?%'; -- Identifying misspelled words
 
+-- Replacing misspelled words
+UPDATE ZomatoData1
+SET City = REPLACE(City, '?', 'i')
+WHERE City LIKE '%?%';
 
--- COUNTRY CODE COLUMN
-SELECT A.[CountryCode],B.COUNTRY
-FROM [dbo].[ZomatoData1] A JOIN [dbo].[ZOMATO_COUNTRY] B
-ON A.[CountryCode] = B.COUNTRYCODE
+-- Counting total restaurants in each city of a particular country
+SELECT COUNTRY_NAME, City, COUNT(City) AS TOTAL_REST
+FROM ZomatoData1
+GROUP BY COUNTRY_NAME, City
+ORDER BY 1, 2, 3 DESC;
 
-ALTER TABLE [dbo].[ZomatoData1] ADD COUNTRY_NAME VARCHAR(50)
+-- Locality column: Rolling count
+SELECT City, Locality, COUNT(Locality) AS COUNT_LOCALITY,
+SUM(COUNT(Locality)) OVER(PARTITION BY City ORDER BY City, Locality) AS ROLL_COUNT
+FROM ZomatoData1
+WHERE COUNTRY_NAME = 'INDIA'
+GROUP BY Locality, City
+ORDER BY 1, 2, 3 DESC;
 
-UPDATE [dbo].[ZomatoData1] SET COUNTRY_NAME = B.COUNTRY								 -- MERGING AND ADDING COUNTRY DETAILS FROM DIFFERENT TABLE THROUGH UPDATE WITH JOIN STATEMENT
-FROM [dbo].[ZomatoData1] A JOIN [dbo].[ZOMATO_COUNTRY] B
-ON A.[CountryCode] = B.[COUNTRYCODE]
+-- Drop columns Address and LocalityVerbose
+ALTER TABLE ZomatoData1 DROP COLUMN Address;
+ALTER TABLE ZomatoData1 DROP COLUMN LocalityVerbose;
 
-SELECT *
-FROM [dbo].[ZomatoData1]
+-- Cuisines column: Counting null or empty entries
+SELECT Cuisines, COUNT(Cuisines)
+FROM ZomatoData1
+WHERE Cuisines IS NULL OR Cuisines = ''
+GROUP BY Cuisines
+ORDER BY 2 DESC;
 
+-- Count cuisines
+SELECT Cuisines, COUNT(Cuisines)
+FROM ZomatoData1
+GROUP BY Cuisines
+ORDER BY 2 DESC;
 
+-- Currency column: Count occurrences
+SELECT Currency, COUNT(Currency)
+FROM ZomatoData1
+GROUP BY Currency
+ORDER BY 2 DESC;
 
---CITY COLUMN
-SELECT DISTINCT [City] FROM [dbo].[ZomatoData1] 
-WHERE CITY LIKE '%?%'																  --IDENTIFYING IF THERE ARE ANY MISS-SPELLED WORD
+-- Yes/No columns
+SELECT DISTINCT Has_Table_booking FROM ZomatoData1;
+SELECT DISTINCT Has_Online_delivery FROM ZomatoData1;
+SELECT DISTINCT Is_delivering_now FROM ZomatoData1;
+SELECT DISTINCT Switch_to_order_menu FROM ZomatoData1;
 
-SELECT REPLACE(CITY,'?','i') 
-FROM [ZomatoData1] WHERE CITY LIKE '%?%'											  --REPLACING MISS-SPELLED WORD
+-- Drop column Switch_to_order_menu
+ALTER TABLE ZomatoData1 DROP COLUMN Switch_to_order_menu;
 
-UPDATE [dbo].[ZomatoData1] SET [City]  = REPLACE(CITY,'?','i') 
-					 FROM [ZomatoData1] WHERE CITY LIKE '%?%'	 			 -- UPDATING WITH REPLACE STRING FUNCTION
+-- Price Range column: Distinct values
+SELECT DISTINCT Price_range FROM ZomatoData1;
 
-SELECT [COUNTRY_NAME], CITY, COUNT([City]) TOTAL_REST							      -- COUNTING TOTAL REST. IN EACH CITY OF PARTICULAR COUNTRY
-FROM [dbo].[ZomatoData1]
-GROUP BY [COUNTRY_NAME],CITY 
-ORDER BY 1,2,3 DESC
+-- Votes column: Checking min, max, and average of votes
+SELECT MIN(CAST(Votes AS INT)) AS MIN_VT, AVG(CAST(Votes AS INT)) AS AVG_VT, MAX(CAST(Votes AS INT)) AS MAX_VT
+FROM ZomatoData1;
 
+-- Cost column: Min, avg, and max of average cost for two
+SELECT Currency, MIN(CAST(Average_Cost_for_two AS INT)) AS MIN_CST,
+AVG(CAST(Average_Cost_for_two AS INT)) AS AVG_CST,
+MAX(CAST(Average_Cost_for_two AS INT)) AS MAX_CST
+FROM ZomatoData1
+GROUP BY Currency;
 
+-- Rating column: Min, avg, and max ratings
+SELECT MIN(Rating), ROUND(AVG(CAST(Rating AS DECIMAL)), 1) AS AVG_RATING, MAX(Rating)
+FROM ZomatoData1;
 
---LOCALITY COLUMN
-SELECT CITY,[Locality], COUNT([Locality]) COUNT_LOCALITY,														-- ROLLING COUNT
-SUM(COUNT([Locality])) OVER(PARTITION BY [City] ORDER BY CITY,[Locality]) ROLL_COUNT
-FROM [dbo].[ZomatoData1]
-WHERE [COUNTRY_NAME] = 'INDIA'
-GROUP BY [Locality],CITY
-ORDER BY 1,2,3 DESC
+SELECT CAST(Rating AS DECIMAL) AS NUM FROM ZomatoData1 WHERE CAST(Rating AS DECIMAL) >= 4;
 
+-- Update Rating column type to DECIMAL
+ALTER TABLE ZomatoData1 MODIFY COLUMN Rating DECIMAL;
 
+-- Select ratings greater than or equal to 4
+SELECT Rating FROM ZomatoData1 WHERE Rating >= 4;
 
---DROP COLUMN,[Locality],[LocalityVerbose][Address]
-ALTER TABLE [dbo].[ZomatoData1] DROP COLUMN [Address]
-ALTER TABLE [dbo].[ZomatoData1] DROP COLUMN [LocalityVerbose] 
+-- Add Rate_Category column based on Rating
+ALTER TABLE ZomatoData1 ADD COLUMN Rate_Category VARCHAR(20);
 
+SELECT Rating,
+CASE
+    WHEN Rating >= 1 AND Rating < 2.5 THEN 'POOR'
+    WHEN Rating >= 2.5 AND Rating < 3.5 THEN 'GOOD'
+    WHEN Rating >= 3.5 AND Rating < 4.5 THEN 'GREAT'
+    WHEN Rating >= 4.5 THEN 'EXCELLENT'
+END AS Rate_Category
+FROM ZomatoData1;
 
-
--- CUISINES COLUMN 
-SELECT [Cuisines], COUNT([Cuisines]) FROM [dbo].[ZomatoData1]
-WHERE [Cuisines] IS NULL OR [Cuisines] = ' '
-GROUP BY [Cuisines]
-ORDER BY 2 DESC
-
-SELECT [Cuisines],COUNT([Cuisines])
-FROM [dbo].[ZomatoData1]
-GROUP BY [Cuisines]
-ORDER BY 2 DESC
-
-
--- CURRENCY COULMN
-SELECT [Currency], COUNT([Currency]) FROM [dbo].[ZomatoData1]
-GROUP BY [Currency]
-ORDER BY 2 DESC
-
-
-
--- YES/NO COLUMNS
-SELECT DISTINCT([Has_Table_booking]) FROM [dbo].[ZomatoData1]
-SELECT DISTINCT([Has_Online_delivery]) FROM [dbo].[ZomatoData1]
-SELECT DISTINCT([Is_delivering_now]) FROM [dbo].[ZomatoData1]
-SELECT DISTINCT([Switch_to_order_menu]) FROM [dbo].[ZomatoData1]
-
-
-
--- DROP COULLMN [Switch_to_order_menu]
-ALTER TABLE [dbo].[ZomatoData1] DROP COLUMN [Switch_to_order_menu]
-
-
-
--- PRICE RANGE COLUMN
-SELECT DISTINCT([Price_range]) FROM [dbo].[ZomatoData1]
-
-
-
--- VOTES COLUMN (CHECKING MIN,MAX,AVG OF VOTE COLUMN)
-ALTER TABLE [dbo].[ZomatoData1] ALTER COLUMN [Votes] INT
-
-SELECT MIN(CAST([Votes] AS INT)) MIN_VT,AVG(CAST([Votes] AS INT)) AVG_VT,MAX(CAST([Votes] AS INT)) MAX_VT
-FROM [dbo].[ZomatoData1]
-
-
-
--- COST COLUMN
-ALTER TABLE [dbo].[ZomatoData1] ALTER COLUMN [Average_Cost_for_two] FLOAT
-
-SELECT [Currency],MIN(CAST([Average_Cost_for_two] AS INT)) MIN_CST,
-AVG(CAST([Average_Cost_for_two] AS INT)) AVG_CST,
-MAX(CAST([Average_Cost_for_two] AS INT)) MAX_CST
-FROM [dbo].[ZomatoData1]
---WHERE [Currency] LIKE '%U%'
-GROUP BY [Currency]
-
-
-
---RATING COLUMN
-SELECT MIN([Rating]),
-ROUND(AVG(CAST([Rating] AS DECIMAL)),1), 
-MAX([Rating])  
-FROM [dbo].[ZomatoData1]
-
-SELECT CAST([Rating] AS decimal) NUM FROM [dbo].[ZomatoData1] WHERE CAST([Rating] AS decimal) >= 4
-
-ALTER TABLE [dbo].[ZomatoData1] ALTER COLUMN [Rating] DECIMAL
-
-SELECT RATING FROM [dbo].[ZomatoData1] WHERE [Rating] >= 4
-
-SELECT RATING,CASE
-WHEN [Rating] >= 1 AND [Rating] < 2.5 THEN 'POOR'
-WHEN [Rating] >= 2.5 AND [Rating] < 3.5 THEN 'GOOD'
-WHEN [Rating] >= 3.5 AND [Rating] < 4.5 THEN 'GREAT'
-WHEN [Rating] >= 4.5 THEN 'EXCELLENT'
-END RATE_CATEGORY
-FROM [dbo].[ZomatoData1]
-
-ALTER TABLE [dbo].[ZomatoData1] ADD RATE_CATEGORY VARCHAR(20)
-
-SELECT * FROM [dbo].[ZomatoData1]
-
-
-
---UPDATING NEW ADDED COLUMN WITH REFFERENCE OF AN EXISTING COLUMN
-UPDATE [dbo].[ZomatoData1] SET [RATE_CATEGORY] = (CASE								     	-- UPDATE WITH CASE-WHEN STATEMENT
-WHEN [Rating] >= 1 AND [Rating] < 2.5 THEN 'POOR'
-WHEN [Rating] >= 2.5 AND [Rating] < 3.5 THEN 'GOOD'
-WHEN [Rating] >= 3.5 AND [Rating] < 4.5 THEN 'GREAT'
-WHEN [Rating] >= 4.5 THEN 'EXCELLENT'
-END)
+-- Update Rate_Category based on Rating
+UPDATE ZomatoData1
+SET Rate_Category = CASE
+    WHEN Rating >= 1 AND Rating < 2.5 THEN 'POOR'
+    WHEN Rating >= 2.5 AND Rating < 3.5 THEN 'GOOD'
+    WHEN Rating >= 3.5 AND Rating < 4.5 THEN 'GREAT'
+    WHEN Rating >= 4.5 THEN 'EXCELLENT'
+END;
